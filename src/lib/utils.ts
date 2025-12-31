759b737d63dc1eef5ae390b54afa72c37456a737
Shaswat21 <codeprogramming101@gmail.com>
2025-12-31 15:54:44 +0530

    Solved: no matching export in "src/lib/utils.ts" for import "cn"

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,22 @@
-import {type CanvasObject } from "./types";
+import { type CanvasObject } from "./types";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
 
 // Convert degrees to radians
 const toRad = (deg: number) => (deg * Math.PI) / 180;
 
 // Rotate a point (x, y) around a center (cx, cy) by an angle in degrees
-export const rotatePoint = (x: number, y: number, cx: number, cy: number, angle: number) => {
+export const rotatePoint = (
+  x: number,
+  y: number,
+  cx: number,
+  cy: number,
+  angle: number
+) => {
   const rad = toRad(angle);
   const cos = Math.cos(rad);
   const sin = Math.sin(rad);
@@ -20,7 +32,7 @@ export const rotatePoint = (x: number, y: number, cx: number, cy: number, angle:
 // This returns the minX, minY, maxX, maxY that fully encloses the rotated item
 export const getRotatedBoundingBox = (obj: CanvasObject) => {
   const { x, y, width, height, rotation } = obj;
-  
+
   if (rotation === 0) {
     return { minX: x, minY: y, maxX: x + width, maxY: y + height };
   }
